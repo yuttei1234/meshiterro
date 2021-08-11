d3c3118fce50b2eaff49080ea0508f1e18b00c12
yuttei1234 <chukyo03210321@yahoo.co.jp>
2021-08-11 01:35:34 +0000

    [Finish]4-8

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -12,5 +12,6 @@ Kaminari.configure do |config|
   # config.max_pages = nil
   # config.params_on_first_page = false
 
-config.default_per_page = 5
+  config.default_per_page = 5
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resource :favroites, only: [:create, :destroy]
-    
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show, :edit, :update]
+  
 end
